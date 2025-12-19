f6f91b2f8f88d4e2db82e42bb75137ecd630021a
Angel Jonguitud <angel.pena5426@alumnos.udg.mx>
2025-12-18 18:37:19 -0600

    feat(api): add /health endpoint

--- dist/app.controller.js
+++ dist/app.controller.js
@@ -11,25 +11,32 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppController = void 0;
 const common_1 = require("@nestjs/common");
-const app_service_1 = require("./app.service");
 let AppController = class AppController {
-    appService;
-    constructor(appService) {
-        this.appService = appService;
+    health() {
+        return {
+            status: 'ok',
+            service: 'cuceiverse-backend',
+            timestamp: new Date().toISOString(),
+        };
     }
-    getHello() {
-        return this.appService.getHello();
+    root() {
+        return 'OK';
     }
 };
 exports.AppController = AppController;
 __decorate([
+    (0, common_1.Get)('health'),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", void 0)
+], AppController.prototype, "health", null);
+__decorate([
     (0, common_1.Get)(),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", []),
-    __metadata("design:returntype", String)
-], AppController.prototype, "getHello", null);
+    __metadata("design:returntype", void 0)
+], AppController.prototype, "root", null);
 exports.AppController = AppController = __decorate([
-    (0, common_1.Controller)(),
-    __metadata("design:paramtypes", [app_service_1.AppService])
+    (0, common_1.Controller)()
 ], AppController);
 //# sourceMappingURL=app.controller.js.map
