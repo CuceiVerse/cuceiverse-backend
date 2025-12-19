e10dad20679fdcfcba821f65fec6d786b078c6f0
Angel Jonguitud <angel.pena5426@alumnos.udg.mx>
2025-12-18 18:50:42 -0600

    feat(api): add /health endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get('health')
+  health() {
+    return {
+      status: 'ok',
+      service: 'cuceiverse-backend',
+      timestamp: new Date().toISOString(),
+    };
+  }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  root() {
+    return 'OK';
   }
 }
