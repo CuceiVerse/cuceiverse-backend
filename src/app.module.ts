d10ff7a770e610c2b1853f575b94954d96c87e13
“Angel <angel.pena5426@alumnos.udg.mx>
2026-02-21 14:08:39 -0600

    fix(health): register AppController

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
+  controllers: [AppController],
 })
 export class AppModule {}
